f577eb9b5bca1523e60adbdbe946c1de6779cac4
TharinduWijayarathna <tharinduwijayarathne87@gmail.com>
2024-05-19 21:33:36 +0530

    Auth Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import './App.css';
 import Login from './pages/auth/login';
 import Home from './pages/home/Home';
 import Employee from './pages/employee/Employee';
+import withAuth from './utils/auth';
+
+const EmployeeWithAuth = withAuth(Employee);
+const HomeWithAuth = withAuth(Home);
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route path='/' element={<HomeWithAuth/>} />
           <Route path='/login' element={<Login />} />
-          <Route path='/employee' element={<Employee />} />
+          <Route path='/employee' element={<EmployeeWithAuth />} />
         </Routes>
       </BrowserRouter>
     </div>
