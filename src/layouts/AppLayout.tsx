776594e3df5499c9b9f22b48969c8e0271204ed1
TharinduWijayarathna <tharinduwijayarathne87@gmail.com>
2024-05-27 19:37:04 +0530

    Readme Updates

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,19 +3,19 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 const AppLayout: React.FC<PropsWithChildren<{}>> = (props) => {
-    return (
-        <>
-        <Navbar />
-        <div className="page">
-          <div className="page-wrapper">
-            <div className="page-body">
-                {props.children}
-            </div>
-            <Footer />
+  return (
+    <>
+      <Navbar />
+      <div className="page">
+        <div className="page-wrapper">
+          <div className="page-body">
+            {props.children}
           </div>
+          <Footer />
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default AppLayout;
